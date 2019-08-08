4b8e6980320ce29a1fd7cc54936131b4c34221d6
JunBi Kim <junbi.kim@navercorp.com>
2019-08-08 14:33:01 +0900

    implements class for request information

--- error_reporting_sdk/src/main/java/com/naver/error_reporting_sdk/ReportInfo.java
+++ error_reporting_sdk/src/main/java/com/naver/error_reporting_sdk/ReportInfo.java
@@ -3,39 +3,74 @@ package com.naver.error_reporting_sdk;
 import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.os.StatFs;
 import android.provider.Settings.Secure;
 
-import com.naver.error_reporting_sdk.log.CustomLog;
-import com.naver.error_reporting_sdk.sender.Sender;
+import com.naver.error_reporting_sdk.log.LogLevel;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ReportInfo {
+public class ReportInfo implements Parcelable {
     private static final int SDK_VERSION = Build.VERSION.SDK_INT;
     private static final String PHONE_BRAND = Build.BRAND;
     private static final String PHONE_MODEL = Build.MODEL;
+    private String androidId;
+    private String packageName;
+    private String logLevel;
+    private String message;
+    private String topOfStackTrace;
+    private Context context;
+    private long availableMemory;
+    private long totalMemory;
 
-    private final String androidId;
-    private final String packageName;
-    private final String message;
-    private final String topOfStackTrace;
-    private final List<CustomLog> customLogs;
-    private final Context context;
-    private final long availableMemory;
-    private final long totalMemory;
-
-    ReportInfo(Builder builder) {
+    private ReportInfo(Builder builder) {
         this.androidId = builder.androidId;
         this.packageName = builder.packageName;
+        this.logLevel = builder.logLevel;
         this.message = builder.message;
         this.topOfStackTrace = builder.topOfStackTrace;
-        this.customLogs = builder.customLogs;
         this.context = builder.context;
         this.availableMemory = builder.getAvailableInternalMemorySize();
         this.totalMemory = builder.getTotalInternalMemorySize();
     }
+
+    private ReportInfo(Parcel parcel) {
+        this.androidId = parcel.readString();
+        this.packageName = parcel.readString();
+        this.logLevel = parcel.readString();
+        this.message = parcel.readString();
+        this.topOfStackTrace = parcel.readString();
+        this.availableMemory = parcel.readLong();
+        this.totalMemory = parcel.readLong();
+    }
+
+    public static final Parcelable.Creator<ReportInfo> CREATOR = new Parcelable.Creator<ReportInfo>() {
+        @Override
+        public ReportInfo createFromParcel(Parcel parcel) {
+            return new ReportInfo(parcel);
+        }
+        @Override
+        public ReportInfo[] newArray(int size) {
+            return new ReportInfo[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(androidId);
+        dest.writeString(packageName);
+        dest.writeString(logLevel);
+        dest.writeString(message);
+        dest.writeString(topOfStackTrace);
+        dest.writeLong(availableMemory);
+        dest.writeLong(totalMemory);
+    }
+
 
     public String getAndroidId() {
         return androidId;
@@ -61,12 +96,12 @@ public class ReportInfo {
         return topOfStackTrace;
     }
 
-    public String getMessage() {
-        return message;
+    public String getLogLevel() {
+        return logLevel;
     }
 
-    public List<CustomLog> getCustomLogs() {
-        return customLogs;
+    public String getMessage() {
+        return message;
     }
 
     public Context getContext() {
@@ -81,32 +116,45 @@ public class ReportInfo {
         return totalMemory;
     }
 
-    public void execute(Sender sender) {
-        sender.send(this);
+    @Override
+    public String toString() {
+        return "ReportInfo{" +
+                "sdkVersion=" + SDK_VERSION +
+                ", phoneBrand='" + PHONE_BRAND + '\'' +
+                ", phoneModel='" + PHONE_MODEL + '\'' +
+                ", androidId='" + androidId + '\'' +
+                ", packageName='" + packageName + '\'' +
+                ", logLevel='" + logLevel + '\'' +
+                ", message='" + message + '\'' +
+                ", topOfStackTrace='" + topOfStackTrace + '\'' +
+                ", context=" + context +
+                ", availableMemory=" + availableMemory +
+                ", totalMemory=" + totalMemory +
+                '}';
     }
 
     static class Builder {
         private final String androidId;
         private final String packageName;
+        private String logLevel;
         private String message;
-        private List<CustomLog> customLogs;
         private Context context;
         private String topOfStackTrace;
 
         public Builder(Context context) {
             this.context = context;
-            this.customLogs = new ArrayList<>();
-            androidId = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
-            packageName = context.getPackageName();
+            this.androidId = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
+            this.packageName = context.getPackageName();
+            this.logLevel = LogLevel.ERROR.getName();
+        }
+
+        public Builder logLevel(LogLevel logLevel) {
+            this.logLevel = logLevel.getName();
+            return this;
         }
 
         public Builder message(String message) {
             this.message = message;
-            return this;
-        }
-
-        public Builder putCustomLog(String key, String value) {
-            customLogs.add(new CustomLog(key, value));
             return this;
         }
 
